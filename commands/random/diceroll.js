5e2179961521c9008f001cae28bfecb5b6d3939f
StudentAlleg <36683605+StudentAlleg@users.noreply.github.com>
2018-02-25 16:53:29 -0800

    Add files via upload

---
+++ commands/random/diceroll.js
@@ -0,0 +1,19 @@
+const commando = require("discord.js-commando");
+
+class DiceRollCommand extends commando.Command {
+    constructor(client){
+        super(client, {
+            name: "roll",
+            group: "random",
+            memberName:"roll",
+            description: "Rolls a die:"
+        });
+    }
+
+    async run(message, arg) {
+        var roll = Math.floor(Math.random() * 6) + 1;
+        message.reply("You roled a " + roll);
+    }
+}
+
+module.exports = DiceRollCommand;
